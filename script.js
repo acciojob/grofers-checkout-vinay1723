86fe58e82ce27f8af5581dde7b09694e58eb289a
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-06-20 14:37:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let totalSum = document.createElement('tr');
+	let table = document.querySelector('table');
+	let prices =  document.querySelectorAll('.price');
+
+	let sum = Array.from(prices).reduce((acc,price) => {
+		return acc + (parseInt(price.textContent) || 0); 
+	},0);
+	totalSum.textContent = `total price ${sum}`;
+	totalSum.setAttribute("colspan",3);
+	totalSum.style.cssText = 'text-align:right';
+	table.appendChild(totalSum);
+    
 };
 
 getSumBtn.addEventListener("click", getSum);
